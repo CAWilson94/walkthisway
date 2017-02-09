2904ef2ea127ec17df73b0933832f7764bfb1b0d
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-02-09 18:09:44 +0000

    goals setters and getters added

--- app/src/main/java/models/Goals.java
+++ app/src/main/java/models/Goals.java
@@ -6,4 +6,42 @@ package models;
 
 public class Goals {
 
+    private String goalName;
+    private int stepsSoFar;
+    private int stepGoal;
+    private Boolean acitve;
+
+    public int getStepsSoFar() {
+        return stepsSoFar;
+    }
+
+    public void setStepsSoFar(int stepsSoFar) {
+        this.stepsSoFar = stepsSoFar;
+    }
+
+    public int getStepGoal() {
+        return stepGoal;
+    }
+
+    public void setStepGoal(int stepGoal) {
+        this.stepGoal = stepGoal;
+    }
+
+    public Boolean getAcitve() {
+        return acitve;
+    }
+
+    public void setAcitve(Boolean acitve) {
+        this.acitve = acitve;
+    }
+
+    public String getGoalName() {
+        return goalName;
+    }
+
+    public void setGoalName(String goalName) {
+        this.goalName = goalName;
+    }
+
+
 }
