560f9aaf8da4e8e3d0a197783fb7a9ce85bb16a7
CAWilson94 <charlottead@live.co.uk>
2017-02-11 20:54:42 +0000

    lisview

--- app/src/main/java/com/example/charl/walkthisway/stats.java
+++ app/src/main/java/com/example/charl/walkthisway/stats.java
@@ -4,12 +4,10 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 
 /**
@@ -67,9 +65,8 @@ public class stats extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_walk,container,false);
-        ListView listView = (ListView) v.findViewById(R.id.list_goals);
-        return v;
+
+        return inflater.inflate(R.layout.fragment_stats, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
