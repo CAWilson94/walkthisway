4c75cd0d75e60d37b88cd61bec6386bc0c8fa0c2
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-02-11 12:26:26 +0000

    dismiss dialog frag

--- app/src/main/java/com/example/charl/walkthisway/addActivity.java
+++ app/src/main/java/com/example/charl/walkthisway/addActivity.java
@@ -1,14 +1,18 @@
 package com.example.charl.walkthisway;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -53,6 +57,7 @@ public class addActivity extends DialogFragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,7 +72,17 @@ public class addActivity extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getDialog().setTitle("Simple Dialog");
-        return inflater.inflate(R.layout.fragment_add_activity, container, false);
+        // Container inflate
+        View v = inflater.inflate(R.layout.fragment_add_activity, container, false);
+        // Dismiss Button
+        Button dismiss = (Button) v.findViewById(R.id.dismiss);
+        dismiss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return v;
     }
 
     @Override
