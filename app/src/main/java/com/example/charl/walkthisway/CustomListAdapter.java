79fa04a06d68d350d65a32ab2a6543e7b2b311a6
CAWilson94 <charlottead@live.co.uk>
2017-02-11 23:55:34 +0000

    arrayadapter with constructor: basic setup

---
+++ app/src/main/java/com/example/charl/walkthisway/CustomListAdapter.java
@@ -0,0 +1,14 @@
+package com.example.charl.walkthisway;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Charlotte on 11/02/2017.
+ */
+
+public class CustomListAdapter extends ArrayAdapter {
+    public CustomListAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+}
