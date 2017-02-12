a84762b935e25c06cd3fa225dc1de949a5882b5f
CAWilson94 <charlottead@live.co.uk>
2017-02-12 12:09:33 +0000

    goalmodel

---
+++ app/src/main/java/Models/Goals.java
@@ -0,0 +1,53 @@
+package Models;
+
+/**
+ * Created by Charlotte on 12/02/2017.
+ */
+
+public class Goals {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumSteps() {
+        return numSteps;
+    }
+
+    public void setNumSteps(int numSteps) {
+        this.numSteps = numSteps;
+    }
+
+    public int getStepTarget() {
+        return stepTarget;
+    }
+
+    public void setStepTarget(int stepTarget) {
+        this.stepTarget = stepTarget;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Boolean getChecked() {
+        return checked;
+    }
+
+    public void setChecked(Boolean checked) {
+        this.checked = checked;
+    }
+
+    String name;
+    int numSteps;
+    int stepTarget;
+    Boolean active;
+    Boolean checked;
+}
