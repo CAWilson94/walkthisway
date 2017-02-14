5312ad7b03e970955eabcc7b9595978b66a23648
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-02-14 12:15:26 +0000

    settings frag

---
+++ app/src/main/java/com/example/charl/walkthisway/Settings.java
@@ -0,0 +1,26 @@
+package com.example.charl.walkthisway;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by charl on 14/02/2017.
+ */
+
+public class Settings extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
